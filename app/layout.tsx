b5e5399148fbb68bc356c0d545dffa9b2498e5d3
Hatim <elsirdesign@gmail.com>
2025-09-01 15:14:44 +0200

    tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,13 +24,37 @@ export default function RootLayout({
     <html lang="fr">
       <head>
         <link rel="icon" type="image/svg+xml" href="/Asteraki_favicon.svg" />
+        
         {/* Prevent iOS from auto-linking */}
         <meta
           name="format-detection"
           content="telephone=no, date=no, email=no, address=no"
         />
+
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TB9X7VPG');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TB9X7VPG"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
         <Toaster />
         <Analytics />
