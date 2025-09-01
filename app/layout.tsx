100631a2d7d756090d6a6ee539a822fabedc1b96
Hatim <elsirdesign@gmail.com>
2025-09-01 19:04:56 +0200

    updated Google Tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,16 +31,6 @@ export default function RootLayout({
           content="telephone=no, date=no, email=no, address=no"
         />
 
-        {/* Google Tag Manager (GTM) */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TB9X7VPG');`,
-          }}
-        />
         {/* End Google Tag Manager */}
 
         {/* Google Ads Conversion Tag (gtag.js) */}
@@ -56,6 +46,18 @@ export default function RootLayout({
           }}
         />
         {/* End Google Ads Conversion Tag */}
+
+        {/* Google Tag Manager (GTM) */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TB9X7VPG');`,
+          }}
+        />
+        
 
       </head>
       <body className={inter.className}>
