61b95d4d54622bd4b155082832f15b3e7c2a3c93
Hatim <elsirdesign@gmail.com>
2025-09-01 18:21:58 +0200

    updated GTAG

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,20 +31,30 @@ export default function RootLayout({
           content="telephone=no, date=no, email=no, address=no"
         />
 
-        {/* Google Ads Conversion Tag (gtag.js) */}
-        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17523897704"></script>
+        {/* Google Tag Manager */}
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'AW-17523897704');
-            `,
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TB9X7VPG');`,
           }}
         />
+        {/* End Google Tag Manager */}
       </head>
       <body className={inter.className}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TB9X7VPG"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        
         {children}
         <Toaster />
         <Analytics />
